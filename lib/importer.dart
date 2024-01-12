d4ca3d9151c054a192722abe7f19b3ea34a2dd2c
uchida189 <ec31088r@nagaoka-ct.ac.jp>
2024-01-12 09:56:34 +0900

    importer削除

--- lib/importer.dart
+++
@@ -1,8 +0,0 @@
-export 'model/item/item_model.dart';
-export 'model/item/item_model_repository.dart';
-
-//export 'ui/home/viewmodel/home_view_model.dart';
-
-export 'ui/home/view/home_page.dart';
-
-export 'main.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'importer.dart';
+import 'ui/home/view/home_page.dart';
 
 void main() async{
   runApp(const ProviderScope(child: MyApp()));
